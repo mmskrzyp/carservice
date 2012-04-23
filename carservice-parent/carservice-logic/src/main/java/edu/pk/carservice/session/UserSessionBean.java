85081c09ac55f7d41d8d080d040a0b192f999198
Mateusz Herych <heniekk@gmail.com>
2012-04-24 01:11:31 +0200

    closing hibernate session after query

--- carservice-parent/carservice-logic/src/main/java/edu/pk/carservice/session/UserSessionBean.java
+++ carservice-parent/carservice-logic/src/main/java/edu/pk/carservice/session/UserSessionBean.java
@@ -14,67 +14,55 @@ import edu.pk.carservice.encryption.PasswordHasher;
 import edu.pk.carservice.entity.User;
 
 public class UserSessionBean {
+	
+	private static final String AUTHENTICATION_QUERY = "from User as user where user.login = ?";
 
 	private SessionFactory sessionFactory;
 	private PasswordHasher passwordHasher;
-	
+
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
-	
+
 	public void setPasswordHasher(PasswordHasher passwordHasher) {
 		this.passwordHasher = passwordHasher;
 	}
 
 	public LoginData authenticate(String login, String password) {
+
 		Session session = sessionFactory.openSession();
-		Query query = session.createQuery("from User as user where user.login = ?");
-		query.setString(0, login);
-		List<User> users = query.list();
-		if(users.size() > 1) {
-			throw new InvalidDbResultException();
-		}
-		boolean authenticated = false;
-		if(users.size() == 1) {
-			User user = users.get(0);
-			String hashedPassword = passwordHasher.encrypt(password);
-			if(user.getPassword().equals(hashedPassword)) {
-				authenticated = true;
-			}
-		}
-		LoginData loginData = new LoginData();
-		
-		loginData.setAuthenticated(authenticated);
-		loginData.setUsername(login);
-		
-		return loginData;
-	}
-	
-	public User getUserbyId(int id) {
-
-		Session session = sessionFactory.openSession(); // u¿ycie
-														// openSession
-		Transaction transaction = null;
-
-		User user = null;
-
 		try {
-			transaction = session.beginTransaction();
+			Query query = session.createQuery(AUTHENTICATION_QUERY);
+			query.setString(0, login);
+			List<User> users = query.list();
+			if (users.size() > 1) {
+				throw new InvalidDbResultException();
+			}
+			boolean authenticated = false;
+			if (users.size() == 1) {
+				User user = users.get(0);
+				String hashedPassword = passwordHasher.encrypt(password);
+				if (user.getPassword().equals(hashedPassword)) {
+					authenticated = true;
+				}
+			}
+			LoginData loginData = new LoginData();
 
-			user = (User) session.get(User.class, id);
+			loginData.setAuthenticated(authenticated);
+			loginData.setUsername(login);
 
-			transaction.commit();
-
-		} catch (HibernateException e) {
-			if (transaction != null)
-				transaction.rollback();
-			e.printStackTrace();
-
-			user = null;
+			return loginData;
 		} finally {
 			session.close();
 		}
+	}
 
+	public User getUserbyId(int id) {
+
+		Session session = sessionFactory.openSession();
+		User user = (User) session.get(User.class, id);
+		session.close();
+		
 		return user;
 	}
 
